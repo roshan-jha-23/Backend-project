2ad70869d4fa1e10ff3257c5322c82a5543f0a68
roshan2345 <jha062305@gmail.com>
2023-12-27 02:37:20 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Error connecting to database",err);
+})
 
 
 
